6ff04b9ab2eb7242b554663b7d788d7281646a88
dedlev <l.popchuk@gmail.com>
2024-05-26 15:58:03 +0300

    adds step 3
    adds step 3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,13 @@ import { nanoid } from 'nanoid';
 
 export class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
     name: '',
     number: '',
   };
@@ -15,7 +21,8 @@ export class App extends Component {
     });
   };
 
-  handleAddContact = () => {
+  handleAddContact = e => {
+    e.preventDefault();
     const { name, number } = this.state;
     if (name && number) {
       const newContact = {
@@ -31,40 +38,54 @@ export class App extends Component {
     }
   };
 
+  getFilteredContacts = () => {
+    const { contacts, filter } = this.state;
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  };
+
   render() {
-    const contacts = this.state.contacts;
+    const filteredContacts = this.getFilteredContacts();
     return (
       <div>
         <h2>Phonebook</h2>
-        <form>
-          <label htmlFor="nameInput">Name </label>
-          <input
-            id="nameInput"
-            type="text"
-            name="name"
-            value={this.state.name}
-            onChange={this.handleChange}
-            required
-          />
+        <form onSubmit={this.handleAddContact}>
+          <label>
+            Name
+            <input
+              type="text"
+              name="name"
+              value={this.state.name}
+              onChange={this.handleChange}
+              required
+            />
+          </label>
 
-          <label htmlFor="numberInput">Number </label>
-          <input
-            id="numberInput"
-            type="tel"
-            name="number"
-            value={this.state.number}
-            onChange={this.handleChange}
-            required
-          />
+          <label>
+            Number
+            <input
+              type="tel"
+              name="number"
+              value={this.state.number}
+              onChange={this.handleChange}
+              required
+            />
+          </label>
 
-          <button type="button" onClick={this.handleAddContact}>
-            Add contact
-          </button>
+          <button type="submit">Add contact</button>
         </form>
 
         <h2>Contacts</h2>
+        <h3>Find contacts by name</h3>
+        <input
+          type="text"
+          name="filter"
+          value={this.state.filter}
+          onChange={this.handleChange}
+        ></input>
         <ul>
-          {contacts.map(contact => (
+          {filteredContacts.map(contact => (
             <li key={contact.id}>
               <p>
                 {contact.name}: {contact.number}
